58c1e91021d198dd85bf4ab0e50aa90d29d0cfb3
Ed Scheinerman <ers@jhu.edu>
2015-10-30 17:57:57 -0400

    Tweaks to iso

--- src/iso.jl
+++ src/iso.jl
@@ -6,36 +6,36 @@ export iso
 An empty matrix is returned if no such `P` can be found.
 """
 function iso(G::SimpleGraph, H::SimpleGraph)
-    m = Model()
     n = NV(G)
 
-    P_fail = Array{Int,2}()
+    err_msg = "The graphs are not isomorphic"
 
-    if NV(H) != n || NE(G) != NE(H)
-        warn("Two graphs have different number of vertices or edges")
-        return P_fail
+    # quickly rule out some easily detected nonisomorphic graphs
+    if NV(H) != n || NE(G) != NE(H) || deg(G) != deg(H)
+        error(err_msg)
     end
 
+    m = Model()
     @defVar(m,P[1:n,1:n],Bin)
-
     A = adjacency(G)
     B = adjacency(H)
 
     for i=1:n
         for k=1:n
             @addConstraint(m,
-                           sum{A[i,j]*P[j,k], j=1:n}
-                           == sum{P[i,j]*B[j,k], j=1:n}
+                           sum{A[i,j]*P[j,k], j=1:n} ==
+                           sum{P[i,j]*B[j,k], j=1:n}
                            )
         end
         @addConstraint(m, sum{P[i,j], j=1:n}==1)
         @addConstraint(m, sum{P[j,i], j=1:n}==1)
     end
 
-    status = solve(m)
-    # println(status)
+    status = solve(m, suppress_warnings=true)
+    
     if status != :Optimal
-        return P_fail
+        error(err_msg)      
     end
+    
     return round(Int,getValue(P))
 end
