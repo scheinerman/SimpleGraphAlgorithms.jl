f27b76b626197311db9d2948e54f0b2be5afe6e0
Ed Scheinerman <ers@jhu.edu>
2016-01-18 14:17:36 -0500

    Extended chromatic polynomials to directed graphs

--- src/chrome_poly.jl
+++ src/chrome_poly.jl
@@ -100,7 +100,9 @@ end
 getindex(CPM,G) = recall(CPM,G)
 
 """
-`chrome_poly(G)` computes the chromatic polynomial of the graph `G`.
+`chrome_poly(G)` computes the chromatic polynomial of the graph `G`. 
+If `G` is a directe graph, this returns the chromatic polynomial of 
+`G`'s underlying simple graph.
 
 This function builds a datastructure to prevent computing the
 chromatic polynomial of the same graph twice. To do this, it uses
@@ -186,3 +188,4 @@ function chrome_poly(G::SimpleGraph) #, CPM::ChromePolyMemo = _CPM)
     return P
 end
 
+chrome_poly(G::SimpleDigraph) = chrome_poly(simplify(G))
