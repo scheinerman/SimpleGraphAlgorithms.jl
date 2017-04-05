ff7bdfbd09bc6e86a6b68999c904c52a2d5157e5
Ed Scheinerman <ers@jhu.edu>
2017-04-05 12:06:23 -0400

    Typo in documentation

--- src/SimpleGraphAlgorithms.jl
+++ src/SimpleGraphAlgorithms.jl
@@ -79,7 +79,7 @@ function max_matching(G::SimpleGraph)
 end
 
 """
-`ver(G)` finds a smallest subset `F` of the edges such that
+`min_edge_cover(G)` finds a smallest subset `F` of the edges such that
 every vertex of `G` is the end point of at least one edge in `F`. An
 error is raised if `G` has a vertex of degree 0.
 """
