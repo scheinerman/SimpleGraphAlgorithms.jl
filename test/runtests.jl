a3fa1a706366bce28662a719c73147b95df3efcf
Ed Scheinerman <ers@jhu.edu>
2024-02-17 15:36:14 -0500

    Switching to GLPK

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,90 +1,105 @@
 using Test
 using SimpleGraphs, SimpleGraphAlgorithms, Polynomials
 
-use_Cbc()
-G = Paley(17)
+# use_GLPK()
+# G = Paley(17)
 
 # Cliques and independent sets
-A = max_clique(G)
-B = max_indep_set(G)
-@test length(A) == length(B)
+@testset "Clique/Indep" begin
+    G = Paley(17)
+    A = max_clique(G)
+    B = max_indep_set(G)
+    @test length(A) == length(B)
+end
 
 # Isomorphism
-f = iso(G, G')
-@test length(f) == 17
-@test is_iso(G, G')
+@testset "Isomorphism" begin
+    G = Paley(17)
+    f = iso(G, G')
+    @test length(f) == 17
+    @test is_iso(G, G')
+
+    # fractional Isomorphism
+    G = Dodecahedron()
+    H = RandomRegular(20, 3)
+    @test is_frac_iso(G, H)
+    H = RandomRegular(20, 4)
+    @test !is_frac_iso(G, H)
+
+    d = hom(Cube(3), Complete(2))
+    @test d["000"] != d["001"]
+end
 
 # Edge matching
-M = max_matching(G)
-@test length(M) == 8
+@testset "Matching" begin
+    G = Paley(17)
+    M = max_matching(G)
+    @test length(M) == 8
 
-d = fractional_matching(Cycle(5))
-@test sum(values(d)) == 5 // 2
 
-H = Dodecahedron()
-X = kfactor(H, 3)
-@test length(X) == 30
+    d = fractional_matching(Cycle(5))
+    @test sum(values(d)) == 5 // 2
+
+    H = Dodecahedron()
+    X = kfactor(H, 3)
+    @test length(X) == 30
+end
 
 # Vertex coloring
-f = vertex_color(G, 6)
-@test length(f) == 17
+@testset "Coloring" begin
+    G = Paley(17)
+    f = vertex_color(G, 6)
+    @test length(f) == 17
 
-# Edge coloring
-@test edge_chromatic_number(G) == 9
-f = edge_color(G, 9)
-@test length(f) == NE(G)
+    # Edge coloring
+    @test edge_chromatic_number(G) == 9
+    f = edge_color(G, 9)
+    @test length(f) == NE(G)
+end
 
 # Domination
-A = min_dom_set(G)
-@test length(A) == 3
+@testset "Domination/Covering" begin
+    G = Paley(17)
+    A = min_dom_set(G)
+    @test length(A) == 3
 
-# Covering
-A = min_vertex_cover(G)
-@test length(A) == 14
-A = min_vertex_cover(G, 10)
-@test length(A) == 2
-A = min_edge_cover(G)
-@test length(A) == 9
+    # Covering
+    A = min_vertex_cover(G)
+    @test length(A) == 14
+    A = min_vertex_cover(G, 10)
+    @test length(A) == 2
+    A = min_edge_cover(G)
+    @test length(A) == 9
 
-# MAD
-@test mad(G) == maximum(deg(G))
+    # MAD
+    @test mad(G) == maximum(deg(G))
+end
 
-# Chromatic Polynomial
-f = chromatic_poly(Cycle(5))
-@test f(2) == 0
-@test f(3) == 30
+@testset "Coloring" begin
+    # Chromatic Polynomial
+    f = chromatic_poly(Cycle(5))
+    @test f(2) == 0
+    @test f(3) == 30
 
-# Chromatic number
-@test chromatic_number(Petersen()) == 3
+    # Chromatic number
+    @test chromatic_number(Petersen()) == 3
 
-G = Spindle()
-add!(G, 4, 0)
-H = Spindle()
-add!(H, 7, 0)
-@test fast_iso_test(G, H)
-f = iso(G, H)
-@test f[0] == 0
-f = iso2(G, H)
-@test f[0] == 0
+    G = Spindle()
+    add!(G, 4, 0)
+    H = Spindle()
+    add!(H, 7, 0)
+    @test fast_iso_test(G, H)
 
-d = hom(Cube(3), Complete(2))
-@test d["000"] != d["001"]
-
-d = vertex_color(Spindle(), 7, 2)
-@test length(d[1] ∩ d[2]) == 0
-
+    d = vertex_color(Spindle(), 7, 2)
+    @test length(d[1] ∩ d[2]) == 0
+end
 
 # edge and vertex connectivity
-G = Complete(8, 8)'
-add!(G, 1, 9)
-add!(G, 1, 10)
-@test edge_connectivity(G) == 2
-@test connectivity(G) == 1
+@testset "Connectivity" begin
+    G = Complete(8, 8)'
+    add!(G, 1, 9)
+    add!(G, 1, 10)
+    @test edge_connectivity(G) == 2
+    @test connectivity(G) == 1
+end
 
-
-# fractional Isomorphism
-G = Dodecahedron()
-H = RandomRegular(20, 3)
-@test is_frac_iso(G, H)
-H = RandomRegular(20, 4)
-@test !is_frac_iso(G, H)
